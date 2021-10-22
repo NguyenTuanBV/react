91bbf20976e42051ab51d3e31ef29be336716bf4
tuan <tuantongthai@gmail.com>
2021-10-22 21:47:02 +0700

    add navigation

--- App.js
+++ App.js
@@ -1,18 +1,46 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Profile from './src/screens/Profile'
 import ListProduct from './src/screens/ListProduct'
 import Product from './src/screens/Product'
 import YourBag from './src/screens/YourBag'
 import Home from './src/screens/Home'
-export default function App() {
+function HomeScreen({ navigation }) {
   return (
-    <View>
-      <Profile />
-      {/* <ListProduct /> */}
-      {/* <Product/> */}
-      {/* <YourBag/> */}
-      {/* <Home/> */}
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
     </View>
-  )
+  );
 }
+
+function DetailsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
